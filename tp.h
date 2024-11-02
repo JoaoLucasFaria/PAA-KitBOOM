616f3323c5267f6b23bd3750a919b0b8f767678b
JoaoLucasFaria <148912535+JoaoLucasFaria@users.noreply.github.com>
2024-11-02 14:50:22 -0300

    add output print and memory/time usage

--- tp.h
+++ tp.h
@@ -2,10 +2,17 @@
 #ifndef TP_H
 #define TP_H
 
+#include "getopt.h"
 #include "grafo.h"
+#include <stdio.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <stdlib.h>
 
-int le_arquivo_composicao(const char *filename, Grafo *g, Completude *completude);
-int le_arquivo_configuracao(const char *filename, Grafo *g);
+int le_arquivo_composicao(const char *filename, Grafo *g, Completude *completude, FILE *saida);
+int le_arquivo_configuracao(const char *filename, Grafo *g, FILE *saida);
 int config_opt(int argc, char *argv[], char **arquivo_composicao, char **arquivo_configuracao);
+int imprime_uso(struct rusage memoria, struct timeval start, struct timeval end, double tempo);
 
 #endif // TP_H
