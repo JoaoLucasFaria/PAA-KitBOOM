706765b6c9a1a32ef99582034ea31988bdb8546e
JoaoLucasFaria <148912535+JoaoLucasFaria@users.noreply.github.com>
2024-11-02 14:49:54 -0300

    add output print and memory/time usage

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 // main.c
-#include <stdio.h>
-#include <stdlib.h>
 #include "getopt.h"
 #include "tp.h"
 #include "grafo.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     char *arquivo_composicao = NULL;
     char *arquivo_configuracao = NULL;
+    struct rusage memoria;
+    struct timeval start, end;
+    double tempo;
+    int e = 0;
+    gettimeofday(&start, NULL);
+    FILE *saida = fopen("Saida", "w");
     config_opt(argc, argv, &arquivo_composicao, &arquivo_configuracao);
 
     Grafo g;
@@ -15,24 +20,31 @@ int main(int argc, char *argv[]) {
     inicializa_grafo(&g, MAX_BARRAS);
 
     // Lê o arquivo de configuração
-    le_arquivo_configuracao(arquivo_configuracao, &g);
+    le_arquivo_configuracao(arquivo_configuracao, &g, &*saida);
     // Lê o arquivo de composição
-    le_arquivo_composicao(arquivo_composicao, &g, &completude);
+    le_arquivo_composicao(arquivo_composicao, &g, &completude, &*saida);
 
     // Verifica a completude
-    if (verificar_completude(&g, &completude)) {
+    if (verificar_completude(&g, &completude))
+    {
         printf("O kit está completo.\n");
-    } else {
-        printf("O kit está incompleto e não poderá ser montado\n");
-        exit(EXIT_FAILURE);
     }
+    else
+    {
+        printf("O kit está incompleto e não poderá ser montado\n");
+        e++;
+    }
+
+
 
     // Verifica o posicionamento
-    if (verificar_posicionamento(&g)) {
+    if (verificar_posicionamento(&g, &*saida) && e != 0)
+    {
         printf("O posicionamento das barras está correto e o Kit pode ser montado\n");
         free_all(&g);
-        exit(EXIT_SUCCESS);
+        fprintf(saida, "Sim\n");
     }
-
-    return 0;
+    else fprintf(saida, "Nao\n");
+    imprime_uso(memoria, start, end, tempo);
+    exit(1);
 }
